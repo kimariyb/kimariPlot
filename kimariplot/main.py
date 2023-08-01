f94b3195aa8073999601c57ee3548c8a4bc0f0ca
kimariyb <kimariyb@163.com>
2023-08-01 14:30:42 +0800

    update v1.1.0

--- kimariplot/main.py
+++ kimariplot/main.py
@@ -4,15 +4,6 @@ from parser import parse
 from poltter import plot_all_line_paths
 
 
-# 定义自定义类型
-def tuple_type(s):
-    try:
-        x, y = map(int, s.split(','))
-        return x, y
-    except:
-        raise argparse.ArgumentTypeError("Invalid tuple format. Please use the format 'x,y'.")
-
-
 def main():
     # 创建 ArgumentParser 对象
     parser = argparse.ArgumentParser(description='Generate a energy profile using kimariplot', add_help=False)
@@ -21,19 +12,20 @@ def main():
     # 添加输入文件参数
     parser.add_argument('input_file', type=str, help='Please input a Toml file')
     # 添加输出文件格式参数
-    parser.add_argument('--output_type', '-o', dest='output', type=str, help='The output type of the graph', default='png')
+    parser.add_argument('--output_type', '-o', dest='output', type=str, help='The output type of the graph',
+                        default='png')
     # 添加输出文件 dpi 参数
     parser.add_argument('--dpi', '-d', dest='dpi', type=int, help='The dpi of the output graph', default=500)
     # 添加全局字体参数
     parser.add_argument('--font', '-f', dest='font', type=str, help='The font family of the graph', default='Arial')
     # 添加图像大小参数
-    parser.add_argument('--size', '-s', dest='size', type=tuple_type, help='The size of the graph', default=(10, 7.5))
+    parser.add_argument('--size', '-s', dest='size', type=str, help='The size of the graph', default='10,7.5')
     # 解析命令行参数
     args = parser.parse_args()
     # 得到 Toml 文件中的数据列表
     plot_data_list = parse(args.input_file)
+    # 将 size 参数转换为元组类型
+    size = tuple(map(float, args.size.split(',')))
     # 绘制所有路径
-    plot_all_line_paths(plot_data_list, args.dpi, args.size, args.font, args.output)
+    plot_all_line_paths(plot_data_list, args.dpi, size, args.font, args.output)
 
-if __name__ == '__main__':
-    main()
--- kimariplot/poltter.py
+++ kimariplot/poltter.py
@@ -10,12 +10,32 @@ def get_suitable_x(plot_data_list):
         mix_list.append(plot_data.get_x())
     # 找到每个列表中的最大值，并将它们存储在一个列表中
     max_values = [max(lst) for lst in mix_list]
-    # # 找到列表中的最大值
+    # 找到列表中的最大值
     max_value = max(max_values)
+
     return max_value
 
 
-def plot_line_path(ax, x, y, labels, color, style):
+def get_suitable_y(plot_data_list):
+    """
+    returns a suitable y
+    """
+    mix_list = []
+    for plot_data in plot_data_list:
+        mix_list.append(plot_data.get_y())
+    # 找到每个列表中的最大值，并将它们存储在一个列表中
+    max_values = [max(lst) for lst in mix_list]
+    # 找到每个列表中的最小值，并将它们存储在一个列表中
+    min_values = [min(lst) for lst in mix_list]
+    # 找到列表中的最大值
+    max_value = max(max_values)
+    # 找到列表中的最小值
+    min_value = min(min_values)
+
+    return max_value, min_value
+
+
+def plot_line_path(ax, x, y, labels, color, style, data_list):
     """
     用来绘制单个路径的 Energy Profile
     :param ax: Matplotlib axes object
@@ -24,15 +44,20 @@ def plot_line_path(ax, x, y, labels, color, style):
     :param labels: List of labels
     :param color: Color of the data
     :param style: Style of the data
+    :param data_list: list of the plot data
     """
+    # 获取合适的 y 增量
+    y_max, y_min = get_suitable_y(data_list)
+    y_add_num = (y_max - y_min) * 0.008
     # 在每个数据点上绘制长度为 0.3 的水平线，并在水平线上显示数字，水平线下显示 label
     for i, (x_new, y_new) in enumerate(zip(x, y)):
         # 绘制水平线
         ax.plot([x_new - 0.15, x_new + 0.15], [y_new, y_new], color=color, linewidth=2.7)
         # 显示数字
-        ax.text(x_new, y_new + 0.5, f"{y_new:.1f}", ha='center', va='bottom', fontsize=8, fontweight='medium')
+        ax.text(x_new, y_new + y_add_num, f"{y_new:.1f}", ha='center', va='bottom', fontsize=8,
+                fontweight='medium')
         # 绘制 label
-        ax.text(x_new, y_new - 1.25, labels[i], ha='center', va='top', fontsize=8.5, fontweight='bold')
+        ax.text(x_new, y_new - y_add_num * 1.8, labels[i], ha='center', va='top', fontsize=8.5, fontweight='bold')
 
     # 以折线连接平台
     for i in range(len(x) - 1):
@@ -62,7 +87,7 @@ def plot_all_line_paths(data_list, dpi, size, font, output_type):
         labels = plot_data.get_labels()
         x = plot_data.get_x()
         y = plot_data.get_y()
-        plot_line_path(ax, x, y, labels, color, style)
+        plot_line_path(ax, x, y, labels, color, style, data_list)
 
     # 找到所有 x_list 中最大的一个值
     x_suitable = get_suitable_x(data_list)
@@ -71,8 +96,8 @@ def plot_all_line_paths(data_list, dpi, size, font, output_type):
     ax.set_xlim(0.5, x_suitable + 0.5)
     # 设置 y 轴标题
     ax.set_ylabel("Gibbs Free Energy (kcal/mol)", fontsize=14, fontweight="bold")
-    # 设置 0.4 留白
-    ax.margins(0.4)
+    # 设置 0.5 留白
+    ax.margins(0.5)
 
     # 将 x 轴和 y 轴的刻度线和标签设置为空
     ax.tick_params(axis='both', which='both', length=0, labelsize=0)
@@ -83,6 +108,3 @@ def plot_all_line_paths(data_list, dpi, size, font, output_type):
     save_name = "figure." + output_type
     # 保存
     fig.savefig(save_name, dpi=dpi)
-
-
-
